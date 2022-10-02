0bfa4a34acc475c19b917cffbbeeeed4f42ba3b8
Calebn8010 <calebn8010@gmail.com>
2022-10-02 09:24:31 -0400

    Remove all body ads

--- popup.js
+++ popup.js
@@ -30,10 +30,23 @@ function setPageBackgroundColor() {
 }
 
 function RemoveAdSearchResults() {
-  var topAds = document.getElementsByClassName('qGXjvb');
-  var sideAds = document.getElementsByClassName('TQc1id hSOk2e rhstc4')
+  console.log(document.getElementsByClassName('qGXjvb'));
+  var bodyAds = document.getElementsByClassName('qGXjvb');
+  console.log(bodyAds.length);
+  //var sideAds = document.getElementsByClassName('TQc1id hSOk2e rhstc4')
+  var sideAds = document.getElementsByClassName('ptJHdc commercial-unit-desktop-rhs VjDLd');
   var bottomAds = document.getElementById('bottomads');
-  topAds[0].parentNode.removeChild(topAds[0]);
-  sideAds[0].parentNode.removeChild(sideAds[0]);
+  //var bottomAds = document.getElementById('ptJHdc commercial-unit-desktop-top');
+  //var tads = document.getElementById('tadsb');
+  
+  // remove all body ads while greater than 0
+  while (bodyAds.length > 0) {
+    bodyAds[0].parentNode.removeChild(bodyAds[0]);
+  }
   bottomAds.remove();
+  //bodyAds[0].parentNode.removeChild(bodyAds[0]);
+  sideAds[0].parentNode.removeChild(sideAds[0]);
+  
+  
+  //tads.remove();
 }
